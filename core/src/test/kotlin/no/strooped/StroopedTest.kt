29434484c997d2a39d5992933bef9bafeed13bd8
Fredrik F Lindhagen <fredrfli@stud.ntnu.no>
2020-03-05 20:32:42 +0100

    Setup junit4 in CI

---
+++ core/src/test/kotlin/no/strooped/StroopedTest.kt
@@ -0,0 +1,16 @@
+package no.strooped
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Guide on how to setup tests in libgdx
+ * http://techduke.io/junit-testing-of-libgdx-game-in-android-studio/
+ * */
+internal class StroopedTest {
+
+    @Test
+    fun `Succeedes`() {
+        assertEquals(true, false)
+    }
+}
