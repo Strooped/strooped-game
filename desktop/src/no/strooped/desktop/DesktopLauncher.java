57226f9b488bc5e49ac2aeb4922a2ee92a3da797
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2020-03-05 18:49:55 +0100

    Install KtLint

---
+++ desktop/src/no/strooped/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.strooped.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.strooped.Strooped;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Strooped(), config);
+	}
+}
