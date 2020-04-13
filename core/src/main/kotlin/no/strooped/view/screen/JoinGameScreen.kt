08f1af7b9cba8d76be3f87cf079b38c728cfb749
Fredrik Førde Lindhagen <Fr3dric0@users.noreply.github.com>
2020-04-13 13:48:16 +0200

    Remove unused imports in JoinGameScreen

--- core/src/main/kotlin/no/strooped/view/screen/JoinGameScreen.kt
+++ core/src/main/kotlin/no/strooped/view/screen/JoinGameScreen.kt
@@ -2,9 +2,7 @@ package no.strooped.view.screen
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
-import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
